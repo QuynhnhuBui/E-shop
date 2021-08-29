15bd2e0882520e02ff5f91d3bf456d052ae3ded2
nhubui <nbui@coinmotion.fi>
2021-08-29 20:10:42 +0300

    fix category id

--- models/category.js
+++ models/category.js
@@ -13,5 +13,14 @@ const categorySchema = mongoose.Schema({
     },
     
 })
+categorySchema.virtual('id').get(function(){
+    return this._id.toHexString()
+})
+
+categorySchema.set('toJSON',{
+    virtuals: true
+})
+
+
 
 exports.Category = mongoose.model('Category', categorySchema);
