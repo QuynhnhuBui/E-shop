344bad1ccaf1cb09de0961598796309487a6c64c
nhubui <nbui@coinmotion.fi>
2021-09-30 13:45:11 +0300

    fix jwt

--- helper/jwt.js
+++ helper/jwt.js
@@ -1,32 +1,29 @@
-const expressJwt = require('express-jwt')
+const expressJwt = require("express-jwt");
 
 function authJwt() {
-    const secret = process.env.secret //JWT_SECRET
-    const api = process.env.API_URL
-    return expressJwt({
-        secret,
-        algorithms: ['HS256'],
-        isRevoked: isRevoked
-    })
-    .unless({ 
-        path:[
-            `${api}/users/login`,
-            `${api}/users/register`,    
-            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/images(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/orders(.*)/ , methods: ['POST', 'OPTIONS'] },
-
-
-        ] 
-    })
+  const secret = process.env.secret; //JWT_SECRET
+  const api = process.env.API_URL;
+  return expressJwt({
+    secret,
+    algorithms: ["HS256"],
+    isRevoked: isRevoked,
+  }).unless({
+    path: [
+      `${api}/users/login`,
+      `${api}/users/register`,
+      `${api}/users/getProfile/:id`,
+      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/images(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/orders(.*)/, methods: ["POST", "OPTIONS"] },
+    ],
+  });
 }
-async function isRevoked(req, payload, done){
-    if(!payload.isAdmin){
-        done(null, true)
-    } else {
-        done()
-    }
-
+async function isRevoked(req, payload, done) {
+  if (!payload.isAdmin) {
+    done(null, true);
+  } else {
+    done();
+  }
 }
-module.exports = authJwt
+module.exports = authJwt;
